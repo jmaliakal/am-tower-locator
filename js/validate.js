95fe908a1b906d27607ff84128dc7c99b6a8479e
awolfe76 <andrew.wolfe@fcc.gov>
2013-10-24 11:56:17 -0400

    validation cleaned up

--- js/validate.js
+++ js/validate.js
@@ -5,58 +5,101 @@ $(function() {
     $(this).next().css("display", "none");
   });
 
-  // validation
-  var latitude = new RegExp('^-?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$');
-  var longitude = new RegExp('^-?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$');
+  function setErrors(input, message) {
+    $(input).parent().addClass("error");
+    $(input).next(".help").css("display", "inline-block");
+    $(input).next(".help").text(message);
+  }
 
+  // validation on blur
   $("#locator input[type=text]").blur(function() {
-    var errorCount = 0;
-
-    if ($(this).hasClass("required")) {
-      if ($(this).val() == "" || $(this).val().length == 0 || $(this).val().length == null) {
-        $(this).parent().addClass("error");
-        $(this).next(".help").css("display", "inline-block");
-        $(this).next(".help").text("Required");
-      } else {
-        if ($(this).hasClass("latitude")) {
-          if (latitude.exec($(this).val()) == null) {
-            $(this).parent().addClass("error");
-            $(this).next(".help").css("display", "inline-block");
-            $(this).next(".help").text("-90.000000 to 90.000000");
-          }
-        }
-
-        if ($(this).hasClass("longitude")) {
-          if (longitude.exec($(this).val()) == null) {
-            $(this).parent().addClass("error");
-            $(this).next(".help").css("display", "inline-block");
-            $(this).next(".help").text("-180.000000 to 180.000000");
-          }
-        }
-
-        if ($(this).hasClass("num")) {
-          if (isNaN($(this).val())) {
-            $(this).parent().addClass("error");
-            $(this).next(".help").css("display", "inline-block");
-            $(this).next(".help").text("Must be a Number");
-          }
-        }
-      }
-    }
-
-    // loop through inputs to see if there are errors
-    $("#locator input[type=text]").each(function() {
-      if ($(this).parent().hasClass("error") || $(this).val() == "" || $(this).val().length == 0 || $(this).val().length == null) {
-        errorCount++;
-      }
-    });
-
-    // if no errors enable button, else disable it
-    if (errorCount == 0) {
-      $('button[type="submit"]').removeAttr('disabled');
-      $('button[type="submit"]').prop('disabled', false);
-    } else {
-      $('button[type="submit"]').prop('disabled', true);
+    var validateInput = input(this); 
+    if (validateInput.message != null) {
+      setErrors(this, validateInput.message);
     }
   });
+
+  // validation on submit
+  $("#search").click(function() {
+    var errors = false;
+    $("#locator input[type=text]").each(function() {
+      var validateInput = input(this);
+      if (validateInput.message != null) {
+        setErrors(this, validateInput.message);
+        errors = true;
+      } 
+    });
+    if (!errors) {
+      $("#locator").submit();
+    }
+  });
+
+  // the input object
+  var input = function(theInput) {
+    var latitudeExp = new RegExp('^-?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$');
+    var longitudeExp = new RegExp('^-?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$');
+
+    var required = function() {
+      if ($(theInput).val() == "" || $(theInput).val().length == 0 || $(theInput).val().length == null) {
+        return true;
+      }
+    };
+
+    var num = function() {
+      if (isNaN($(theInput).val())) {
+        return true;
+      }
+    };
+
+    var latitude = function() {
+      if (latitudeExp.exec($(theInput).val()) == null) {
+        return true;
+      }
+    };
+
+    var longitude = function() {
+      if (longitudeExp.exec($(theInput).val()) == null) {
+        return true;
+      }
+    };
+
+    var isValid = function() {
+      var errorMessage = null;
+
+      if ($(theInput).hasClass("required")) {
+        if(required()) {
+          errorMessage = 'Required';
+          return errorMessage;
+        }
+      }
+
+      if ($(theInput).hasClass("num")) {
+        if(num()) {
+          errorMessage = 'Must be a Number';
+          return errorMessage;
+        }
+      }
+
+      if ($(theInput).hasClass("latitude")) {
+        if(latitude()) {
+          errorMessage = '-90.000000 to 90.000000';
+          return errorMessage;
+        }
+      }
+
+      if ($(theInput).hasClass("longitude")) {
+        if(longitude()) {
+          errorMessage = '-180.000000 to 180.000000';
+          return errorMessage;
+        }
+      }
+
+      return errorMessage;
+    };
+
+    // public
+    return {
+      message: isValid()
+    };
+  } 
 });
