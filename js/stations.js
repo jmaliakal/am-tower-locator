dde9cbd2b043a84b9789de9f6b47341558adfdcb
awolfe76 <andrew.wolfe@fcc.gov>
2013-10-24 20:50:09 -0400

    stations headings change; more js cleanup;

--- js/stations.js
+++ js/stations.js
@@ -1,7 +1,7 @@
 // callback function
 function stations_output(stations) {
   // removes loading screen after we get results
-  $('#station-notify, #station-dont-notify').html("");
+  $('#stations').html("");
 
   // set up icons
   var towerIcon = L.icon({
@@ -24,38 +24,45 @@ function stations_output(stations) {
     $.get('templates/stations.html', function(templates) {
       var template = $(templates).filter('#stations-none').html();
       var html = Mustache.to_html(template);
-      $(html).hide().appendTo("#station-notify").fadeIn(1000);
+      $(html).hide().appendTo("#stations");
     });
   } else {  // yes results
     // loop through api results
     $.each(stations.stations, function(key, curStation) {
       var modStation = new station(curStation, $("#height").val());
+      curStation.isCritical = modStation.notify();
 
+      /*
       // APpend modified result
       $.get('templates/stations.html', function(templates) {
         var template = $(templates).filter('#station-detail').html();
         var html = Mustache.to_html(template, curStation);
         $(html).appendTo("#station-" + modStation.notify());
       });
-
+      
       // add the map
       // interval required to make sure the map container exists
       var checkExist = setInterval(function() {
          if ($('#st'+curStation.facid+'-'+curStation.hours_operation+'-map').length) {
             // set up map for a station
             var stationMap = L.mapbox.map('st'+curStation.facid+'-'+curStation.hours_operation+'-map', 'fcc.map-fd8wksyc', {zoomControl: false}).setView([curStation.decimal_lat_nad83, curStation.decimal_lon_nad83], 12);
+            // no need for zoom, etc
+            stationMap.dragging.disable();
+            stationMap.touchZoom.disable();
+            stationMap.doubleClickZoom.disable();
+            stationMap.scrollWheelZoom.disable();
 
             // add proposed tower marker
             L.marker([$("#lat").val(), $("#long").val()], {
               icon: towerIcon
             }).bindPopup("<strong>YOUR TOWER.<strong>").addTo(stationMap);
 
-            // set up am tower icon
+            // set up AM tower icon
             var amIcon = notifyIcon;
             if (modStation.notify() == 'dont-notify') {
               amIcon = dontNotifyIcon;
             }
-            // add AM tower marker
+            // add AM tower marker, with popup
             L.marker([curStation.decimal_lat_nad83, curStation.decimal_lon_nad83], {
               icon: amIcon,
               title: "st" + curStation.facid,
@@ -66,29 +73,22 @@ function stations_output(stations) {
               }
             }).bindPopup("<strong>"+curStation.call+"</strong>").addTo(stationMap);
 
-            stationMap.dragging.disable();
-            stationMap.touchZoom.disable();
-            stationMap.doubleClickZoom.disable();
-            stationMap.scrollWheelZoom.disable();
-
             // clear interval
             clearInterval(checkExist);
          }
       }, 100); // check every 100ms
+    */
     }); // end each
 
+      $.get('templates/stations.html', function(templates) {
+        var template = $(templates).filter('#station-detail-new').html();
+        var html = Mustache.to_html(template, stations);
+        $(html).appendTo('#stations');
+      });
+  
     // add headings
-    $.get('templates/stations.html', function(templates) {
-      var template = $(templates).filter('#stations-notify-heading').html();
-      var html = Mustache.to_html(template);
-      $(html).prependTo("#station-notify");
-    });
-
-    $.get('templates/stations.html', function(templates) {
-      var template = $(templates).filter('#stations-dont-notify-heading').html();
-      var html = Mustache.to_html(template);
-      $(html).prependTo("#station-dont-notify");
-    });
+    //$('<h4 class="notify-heading">You <span>must</span> notify the following stations:</h4><br />').prependTo("#station-notify");
+    //$('<h4 class="dont-notify-heading">You are <span>not</span> required to notify following stations:</h4><br />').prependTo("#station-dont-notify");
 
     $('html,body').animate({
       scrollTop: $(".content").offset().top
@@ -101,7 +101,8 @@ function stations_output(stations) {
 var station = function(curStation, height) {
   // is tower directional
   var isNonDirectional = function() {
-    if (curStation.ant_mode.substring(0, 3) === "NDD") {
+    console.log ('tower count = ' + curStation.tower_count);
+    if (curStation.tower_count == 1) {
       return true;
     }
   };
